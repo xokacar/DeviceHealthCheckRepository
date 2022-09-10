2dd448336240c87510eb2693ddb6cb233871c587
Okay Kaçar <okaykacar31@gmail.com>
2022-09-10 17:21:41 +0300

    Feat:DVH-1 Login Screen created and implemented

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, StyleSheet, TextInput, Text } from "react-native";
+import { Input } from "../components/Input";
+
+export const LoginScreen = () => {
+    return (
+        <View>
+            <Input />
+        </View>
+    );
+};
