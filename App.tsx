a247acd62ebe5382eeaaa35149c936ca3378a3ba
Okay Kaçar <okaykacar31@gmail.com>
2022-09-20 00:08:17 +0300

    Fix:DVH-2 / Import update for the App

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { LoginScreen } from './src/screens/LoginScreen';
+import { LoginScreen } from './src/screens/Login/LoginScreen';
 
 export default function App() {
   return (
