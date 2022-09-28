e780e3296c8d0300e362d3f7c94da615443b0704
Okay Kaçar <okaykacar31@gmail.com>
2022-09-29 00:35:07 +0300

    Feat:DVH-3/ ThemeColors Updated

--- src/core/theme.tsx
+++ src/core/theme.tsx
@@ -4,8 +4,8 @@ export const theme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    primary: '#600EE6',
-    secondary: '#414757',
+    primary: '#00897b',
+    secondary: '#00796b',
     error: '#f13a59',
   },
 };
