00d6528f23b7f5b0a4737f14c883e0aa5078e6ef
Okay Kaçar <okaykacar31@gmail.com>
2022-10-08 20:20:13 +0300

    Feat:DVH-4/ Tab Navigator updated with icons

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,44 @@ import {LoginScreen} from './screens/Login/LoginScreen'
 import { SignupScreen } from './screens/SignUp/SignupScreen';
 import { DashboardScreen } from './screens/Dashboard/DashboardScreen';
 import { OrganizationPanelScreen } from './screens/OrganizationPanel/OrganizationPanel';
+import { MyAccountScreen } from './screens/MyAccount/MyAccountScreen';
+import {Ionicons} from '@expo/vector-icons'
+import { theme } from './core/theme';
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export const Navigator = () => {
 
-    const isLogged = false;
+    const isLogged = true;
 
     if (isLogged) {
         return (
-            <Tab.Navigator screenOptions={ {headerShown:false} }>
+            <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+    
+                if (route.name === 'Dashboard') {
+                  iconName = focused
+                    ? 'server'
+                    : 'server-outline';
+                } else if (route.name === 'Organization') {
+                  iconName = focused ? 'at' : 'at-outline';
+                }
+                else if (route.name === 'My Account') {
+                    iconName = focused ? 'person' : 'person-outline';
+                    }
+    
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: theme.colors.primary,
+              tabBarInactiveTintColor: 'gray',
+            })}
+          >
             <Tab.Screen name="Dashboard" component={DashboardScreen}   />
             <Tab.Screen name='Organization' component={OrganizationPanelScreen}/>
+            <Tab.Screen name='My Account' component={MyAccountScreen} />
             </Tab.Navigator>
         );
     }
