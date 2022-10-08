7c64d87e7ba6bcb323fa6b567b7d3b999ec88c33
Okay Kaçar <okaykacar31@gmail.com>
2022-10-08 20:25:34 +0300

    Feat:DVH-4/ MyAccount Screen and Style

---
+++ src/screens/MyAccount/MyAccountScreen.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { SafeAreaView } from "react-native";
+import { MyAccountStyle } from "./styles";
+import { List } from 'react-native-paper';
+import {theme} from '../../core/theme'
+
+
+
+export const MyAccountScreen = () => {
+    const styles = MyAccountStyle();
+    return (
+        <SafeAreaView
+        style={styles.container}
+        >
+            <List.Section>
+                <List.Subheader> My Account </List.Subheader>
+            <List.Accordion
+            title="Edit Credentials"
+            description="Click to change your credentials"
+            left={props => <List.Icon {...props} icon="lock" color={theme.colors.primary} />}
+            >
+                <List.Item
+            title="Change Name / Surname"
+            description="Click to change your credentials"
+            left={props => <List.Icon {...props} icon="lock" color={theme.colors.primary} />}
+            />
+            <List.Item
+            title="Change Password"
+            description="Click to change your credentials"
+            left={props => <List.Icon {...props} icon="lock" color={theme.colors.primary} />}
+            />
+            <List.Item
+            title="Change Phone Number"
+            description="Click to change your credentials"
+            left={props => <List.Icon {...props} icon="lock" color={theme.colors.primary} />}
+            />
+            </List.Accordion>
+            </List.Section>
+        </SafeAreaView>
+    );
+};
---
+++ src/screens/MyAccount/styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+
+export const MyAccountStyle = () => {
+    return StyleSheet.create({
+        container : {
+            flex: 1,
+            width: '100%',
+        },
+    });
+};
