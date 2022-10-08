340d08ab669a81aa8c3aee4dab81d939ddcad676
Okay Kaçar <okaykacar31@gmail.com>
2022-10-08 20:24:05 +0300

    Feat:DVH-4/ Search bar  working  (title/ip search function)

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,30 +1,41 @@
-import React, {useState} from 'react';
-import { SafeAreaView, View, TextInput, Keyboard } from 'react-native';
+import React from 'react';
+import { SafeAreaView, TextInput, Keyboard } from 'react-native';
 import { SearchBarStyle } from './styles';
 import {Ionicons} from '@expo/vector-icons'
 import { theme } from '../../core/theme';
 import { Button } from 'react-native-paper';
 
 
-export const SearchBar = () => {
+export const SearchBar = ( {clicked, searchPhrase, setSearchPhrase, setClicked} )  => {
     const styles = SearchBarStyle();
-    const [searchPhrase, setSearchPhrase] = useState('');
-    const [clicked, setClicked] = useState(false);
-
+    
     return (
         <SafeAreaView style={styles.container}>
+        
         <Ionicons 
         name="md-search-outline" size={24} 
         color={theme.colors.secondary} 
-        style={{marginRight:1}} />
-        <TextInput 
-        style={styles.input}placeholder="Search"
+        style={{marginRight:1}} 
         />
+        
+        <TextInput 
+        style={styles.input}
+        placeholder="Search"
+        value={searchPhrase}
+        onChangeText={setSearchPhrase}
+        onFocus={() => {
+          setClicked(true);
+        }}
+        />
+        
         <Button
-        onPress={Keyboard.dismiss}
+        onPress={() => {
+          Keyboard.dismiss();
+          setClicked(false);
+        } }
         children={'cancel'}
         />
+        
         </SafeAreaView>
     );
 };
-
